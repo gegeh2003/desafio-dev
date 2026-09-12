5f4121704f7ba3638908fabac866ffa3c98ec8a4
Geovana <geovanaandrade507@gmail.com>
2026-09-12 15:26:20 -0300

    Criar tabela da agenda e atualiza script inicial

--- src/main/resources/CRIA_TABELAS_E_INSERE_REGISTROS_INICIAIS.sql
+++ src/main/resources/CRIA_TABELAS_E_INSERE_REGISTROS_INICIAIS.sql
@@ -1,2 +1,4 @@
 CREATE TABLE funcionario (rowid bigint auto_increment, nm_funcionario VARCHAR(255));
 INSERT INTO funcionario (nm_funcionario) VALUES ('João'), ('Maria'), ('José'), ('Joana');
+
+CREATE TABLE agenda (rowid bigint auto_increment, nm_agenda VARCHAR(255), periodo_disponivel VARCHAR(255));
