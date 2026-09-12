e5769286aebaf5a2cc09ebf48295c6c2fee6174a
Geovana <geovanaandrade507@gmail.com>
2026-09-12 15:20:52 -0300

    Implementar regras de negócio e dados da agenda

--- src/main/java/br/com/soc/sistema/dao/AgendaDao.java
+++ src/main/java/br/com/soc/sistema/dao/AgendaDao.java
@@ -10,7 +10,6 @@ import java.util.Collections;
 import java.util.List;
 
 import br.com.soc.sistema.vo.AgendaVo;
-import br.com.soc.sistema.vo.FuncionarioVo;
 
 public class AgendaDao extends Dao {
 
@@ -41,7 +40,7 @@ public class AgendaDao extends Dao {
 	    try (
 	        Connection con = getConexao();
 	        PreparedStatement ps = con.prepareStatement(query.toString())
-	    ) {
+	    ){
 	        int i = 1;
 
 	        ps.setString(i++, agendaVo.getNome());
@@ -57,7 +56,7 @@ public class AgendaDao extends Dao {
 	
 	public List<AgendaVo> findAllAgendas(){
 		
-		StringBuilder query = new StringBuilder("SELECT rowid id, nm_agenda nome, periodo_disponivel disponivel FROM agenda");
+		StringBuilder query = new StringBuilder("SELECT rowid id, nm_agenda nome, periodo_disponivel periodo FROM agenda");
 		
 		try(
 			Connection con = getConexao();
@@ -83,5 +82,58 @@ public class AgendaDao extends Dao {
 		
 		return Collections.emptyList();
 	}
-}
+	
+	public AgendaVo findByCodigo(Integer codigo) {
+		
+		StringBuilder query = new StringBuilder ("SELECT rowid id, nm_agenda nome, periodo_disponivel periodo FROM agenda ").append("WHERE rowid = ?");
+		
+		try(
+			Connection con = getConexao();
+		    PreparedStatement ps = con.prepareStatement(query.toString())
+			){
+			
+			int i= 1;
+			ps.setInt(i, codigo);
+			
+			 try(ResultSet rs = ps.executeQuery()){
+
+		            AgendaVo vo = null;
+
+		            while(rs.next()){
+
+		                vo = new AgendaVo();
+
+		                vo.setRowid(rs.getString("id"));
+		                vo.setNome(rs.getString("nome"));
+		                vo.setPeriodoDisponivel(rs.getString("periodo"));
+		            }
+
+		            return vo;
+		        }
+
+		    } catch (SQLException e) {
+		        e.printStackTrace();
+		  }
+
+		    return null;
+		}
+	
+	public void excluirAgenda(String rowid) {
+	StringBuilder query = new StringBuilder("DELETE FROM agenda WHERE rowid = ?");
+		
+		try (
+			Connection con =getConexao();
+			PreparedStatement ps = con.prepareStatement(query.toString())
+			){
+				int i = 1;
+				ps.setString(i++, rowid);
+				ps.executeUpdate();
+				
+		} catch (SQLException e) {
+			e.printStackTrace();
+			}
+		}
+	
+	}
+
 
