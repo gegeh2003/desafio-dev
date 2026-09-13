54ce8001da6cb5d25181b4503a0f47ca34f2cfb9
Geovana <geovanaandrade507@gmail.com>
2026-09-13 19:07:54 -0300

    Finalizar CRUD de compromissos

--- src/main/java/br/com/soc/sistema/business/RelatorioCompromissoBusiness.java
+++ src/main/java/br/com/soc/sistema/business/RelatorioCompromissoBusiness.java
@@ -7,26 +7,25 @@ import br.com.soc.sistema.exception.BusinessException;
 import br.com.soc.sistema.vo.RelatorioCompromissoVo;
 
 public class RelatorioCompromissoBusiness {
-	
+
 	private RelatorioCompromissoDao dao;
-	
+
 	public RelatorioCompromissoBusiness() {
 		this.dao = new RelatorioCompromissoDao();
 	}
-	
+
 	public List<RelatorioCompromissoVo> buscarPorPeriodo(String dataInicial, String dataFinal) {
-		
-		if (dataInicial == null || dataInicial.isEmpty()
-			|| dataFinal == null || dataFinal.isEmpty()) {
-			
+
+		if (dataInicial == null || dataInicial.isEmpty() || dataFinal == null || dataFinal.isEmpty()) {
+
 			throw new BusinessException("Data inicial e data final sao obrigatorias");
 		}
-		
-		if (dataInicial.compareTo(dataFinal)> 0) {
+
+		if (dataInicial.compareTo(dataFinal) > 0) {
 			throw new BusinessException("A data inicial não pode ser maior que a data final");
-			
+
 		}
-		
+
 		return dao.buscarPorPeriodo(dataInicial, dataFinal);
 	}
 }
