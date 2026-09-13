9bd83c3a969f96f1b85c82c281fc2a4e4729a293
Geovana <geovanaandrade507@gmail.com>
2026-09-13 01:48:07 -0300

    Implementar não permitir que sejam cadastrados compromissos fora da disponibilidade da agenda

--- src/main/java/br/com/soc/sistema/business/CompromissoBusiness.java
+++ src/main/java/br/com/soc/sistema/business/CompromissoBusiness.java
@@ -3,9 +3,10 @@ package br.com.soc.sistema.business;
 import java.util.List;
 
 import br.com.soc.sistema.dao.CompromissoDao;
-import br.com.soc.sistema.vo.AgendaVo;
 import br.com.soc.sistema.vo.CompromissoVo;
 import br.com.soc.sistema.exception.BusinessException;
+import br.com.soc.sistema.vo.AgendaVo;
+import java.time.LocalTime;
 
 public class CompromissoBusiness {
 	private static final String FOI_INFORMADO_CARACTER_NO_LUGAR_DE_UM_NUMERO = "Foi informado um caracter no lugar de um numero";
@@ -23,14 +24,19 @@ public class CompromissoBusiness {
 
 		try {
 
-			if (compromissoVo.getCodigoFuncionario().isEmpty() || compromissoVo.getCodigoAgenda().isEmpty()
-					|| compromissoVo.getData().isEmpty() || compromissoVo.getHorario().isEmpty()) {
+			if (compromissoVo.getCodigoFuncionario().isEmpty() 
+					|| compromissoVo.getCodigoAgenda().isEmpty()
+					|| compromissoVo.getData().isEmpty() 
+					|| compromissoVo.getHorario().isEmpty()) {
 
 				throw new IllegalArgumentException("Todos os campos sao obrigatorios");
 			}
 
+			validarHorario(compromissoVo);
 			dao.insertCompromisso(compromissoVo);
 
+		} catch (BusinessException e) {
+			throw e;
 		} catch (Exception e) {
 			e.printStackTrace();
 			throw new BusinessException("Nao foi possivel realizar a inclusao do registro");
@@ -38,6 +44,7 @@ public class CompromissoBusiness {
 	}
 
 	public void atualizarCompromisso(CompromissoVo compromissoVo) {
+		validarHorario(compromissoVo);
 		dao.atualizarCompromisso(compromissoVo);
 	}
 
@@ -54,5 +61,34 @@ public class CompromissoBusiness {
 
 	public void excluirCompromisso(String rowid) {
 		dao.excluirCompromisso(rowid);
+	}
+
+	public void validarHorario(CompromissoVo compromissoVo) {
+		AgendaBusiness agendaBusiness = new AgendaBusiness();
+
+		AgendaVo agenda = agendaBusiness.buscarAgendaPor(compromissoVo.getCodigoAgenda());
+
+		String periodo = agenda.getPeriodoDisponivel();
+
+		LocalTime horario = LocalTime.parse(compromissoVo.getHorario());
+
+		LocalTime inicio;
+		LocalTime fim;
+
+		if ("Manhã".equals(periodo)) {
+			inicio = LocalTime.of(6, 0);
+			fim = LocalTime.of(11, 59);
+
+		} else if ("Tarde".equals(periodo)) {
+			inicio = LocalTime.of(12, 0);
+			fim = LocalTime.of(18, 0);
+
+		} else {
+			inicio = LocalTime.of(6, 0);
+			fim = LocalTime.of(18, 0);
+		}
+		if (horario.isBefore(inicio) || horario.isAfter(fim)) {
+			throw new BusinessException("O horario informado nao esta disponivel para a agenda");
+		}
 	}
 }
