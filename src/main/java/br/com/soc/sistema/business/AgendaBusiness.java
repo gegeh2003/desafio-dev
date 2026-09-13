e5abed1c6395285d58782d6526c2c6dca4e2d8fc
Geovana <geovanaandrade507@gmail.com>
2026-09-13 19:08:32 -0300

    Finalizar CRUD de agendas

--- src/main/java/br/com/soc/sistema/business/AgendaBusiness.java
+++ src/main/java/br/com/soc/sistema/business/AgendaBusiness.java
@@ -6,49 +6,47 @@ import br.com.soc.sistema.dao.AgendaDao;
 import br.com.soc.sistema.exception.BusinessException;
 import br.com.soc.sistema.vo.AgendaVo;
 
-
-	public class AgendaBusiness {
+public class AgendaBusiness {
 	private static final String FOI_INFORMADO_CARACTER_NO_LUGAR_DE_UM_NUMERO = "Foi informado um caracter no lugar de um numero";
 	private AgendaDao dao;
-	
+
 	public AgendaBusiness() {
 		this.dao = new AgendaDao();
 	}
-	
+
 	public List<AgendaVo> trazerTodasAsAgendas() {
-	    return dao.findAllAgendas();
+		return dao.findAllAgendas();
 	}
-	
+
 	public void salvarAgenda(AgendaVo agendaVo) {
-		
+
 		try {
-			if(agendaVo.getNome().isEmpty())
-				throw new IllegalArgumentException("Nome nao pode ser em branco");
-			
+			if (agendaVo.getNome().isEmpty())
+				throw new IllegalArgumentException("Nome não pode ser em branco");
+
 			dao.insertAgenda(agendaVo);
 		} catch (Exception e) {
-			throw new BusinessException("Nao foi possivel realizar a inclusao do registro");
+			throw new BusinessException("Não foi possível realizar a inclusão do registro");
 		}
-		
-	}	
-	
+
+	}
+
 	public void atualizarAgenda(AgendaVo agendaVo) {
 		dao.atualizarAgenda(agendaVo);
 	}
-	
+
 	public AgendaVo buscarAgendaPor(String codigo) {
-		
+
 		try {
 			Integer cod = Integer.parseInt(codigo);
 			return dao.findByCodigo(cod);
-			
-		}catch (NumberFormatException e) {
+
+		} catch (NumberFormatException e) {
 			throw new BusinessException(FOI_INFORMADO_CARACTER_NO_LUGAR_DE_UM_NUMERO);
 		}
 	}
-	
+
 	public void excluirAgenda(String rowid) {
 		dao.excluirAgenda(rowid);
 	}
 }
-
