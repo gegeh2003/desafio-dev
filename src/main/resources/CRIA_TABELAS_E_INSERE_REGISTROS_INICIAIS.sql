edd43774b81deace7fe78733f82c7acf08a4c665
Geovana <geovanaandrade507@gmail.com>
2026-09-12 22:07:25 -0300

    Configurra action no struts e implementa insert da agenda

--- src/main/resources/CRIA_TABELAS_E_INSERE_REGISTROS_INICIAIS.sql
+++ src/main/resources/CRIA_TABELAS_E_INSERE_REGISTROS_INICIAIS.sql
@@ -2,3 +2,6 @@ CREATE TABLE funcionario (rowid bigint auto_increment, nm_funcionario VARCHAR(25
 INSERT INTO funcionario (nm_funcionario) VALUES ('João'), ('Maria'), ('José'), ('Joana');
 
 CREATE TABLE agenda (rowid bigint auto_increment, nm_agenda VARCHAR(255), periodo_disponivel VARCHAR(255));
+INSERT INTO agenda (nm_agenda, periodo_disponivel) VALUES ('Ultrassonografia abdominal','Manhã'), ('Endoscopia digestiva alta','Tarde'), ('Consulta de Gastroenterologista','Ambos');
+
+CREATE TABLE compromisso (rowid bigint auto_increment, cd_funcionario bigint, cd_agenda bigint, data DATE, horario TIME);
