c1c0ceee8d577ccd5236272c950c3d35695f5e7e
Geovana <geovanaandrade507@gmail.com>
2026-09-13 14:30:27 -0300

    Implementar exportação Excel ao relatório de compromissos

--- src/main/java/br/com/soc/sistema/action/RelatorioCompromissoAction.java
+++ src/main/java/br/com/soc/sistema/action/RelatorioCompromissoAction.java
@@ -6,14 +6,20 @@ import java.util.List;
 import br.com.soc.sistema.business.RelatorioCompromissoBusiness;
 import br.com.soc.sistema.infra.Action;
 import br.com.soc.sistema.vo.RelatorioCompromissoVo;
+import br.com.soc.sistema.relatorio.ExportadorExcel;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 
 public class RelatorioCompromissoAction extends Action {
 	
 	private List<RelatorioCompromissoVo> relatorio = new ArrayList<>();
 	private RelatorioCompromissoBusiness business = new RelatorioCompromissoBusiness();
+	private ExportadorExcel exportadorExcel = new ExportadorExcel();
 	
 	private String dataInicial;
 	private String dataFinal;
+	
+	private InputStream inputStream;
 	
 	public String tela() {
         return SUCCESS;
@@ -22,6 +28,15 @@ public class RelatorioCompromissoAction extends Action {
 	public String gerar() {
 		
 		relatorio = business.buscarPorPeriodo(dataInicial, dataFinal);
+		
+		return SUCCESS;
+	}
+	
+	public String exportar() {
+		
+		relatorio = business.buscarPorPeriodo(dataInicial, dataFinal);
+		
+		inputStream = new ByteArrayInputStream(exportadorExcel.gerarExcel(relatorio));
 		
 		return SUCCESS;
 	}
@@ -48,5 +63,9 @@ public class RelatorioCompromissoAction extends Action {
 
     public void setDataFinal(String dataFinal) {
         this.dataFinal = dataFinal;
+    }
+    
+    public InputStream getInputStream() {
+        return inputStream;
     }
 }
